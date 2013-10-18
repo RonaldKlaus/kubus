5cf1aa473cfb6c3d2e671e66e4b82063956204b2
Ronald Klaus <ronald.klaus@absolventa.de>
2013-10-18 10:58:58 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131018084342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "title"
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "email"
+    t.boolean  "gender"
+    t.string   "street"
+    t.datetime "birth_date"
+    t.string   "location"
+    t.string   "zipcode"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
