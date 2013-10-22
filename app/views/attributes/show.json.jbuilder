c2201948b83f786c96ac15f76382b1aeba4d83b7
Ronald Klaus <ronald.klaus@absolventa.de>
2013-10-22 17:53:49 +0200

    add attribute

---
+++ app/views/attributes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @attribute, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 KubusApp::Application.routes.draw do
   resources :attribute_languages
 
+  resources :attributes
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
