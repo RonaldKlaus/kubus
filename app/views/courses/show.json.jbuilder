e3369e45c25e8a5b4685d6643e3b7f912265a98c
Ronald Klaus <ronald.klaus@absolventa.de>
2013-10-24 09:28:39 +0200

    add Course

---
+++ app/views/courses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @course, :number, :name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ KubusApp::Application.routes.draw do
 
   resources :sciencedomain_languages
 
+  resources :courses
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
